1def4ccec0f8dca54185303ad02475d218daccf0
3Ravaka <120034136+3Ravaka@users.noreply.github.com>
2023-10-23 11:15:51 +0200

    Create Manche.java

---
+++ MancheRisk/src/Manche.java
@@ -0,0 +1,11 @@
+public class Manche{
+    //attribute 
+    private Plateau plateau;
+    private int nbEchange;
+    //constructor 
+    public Manche(int nbJoueur){
+        débuterManche()
+        setOrdre()
+    }
+
+}
