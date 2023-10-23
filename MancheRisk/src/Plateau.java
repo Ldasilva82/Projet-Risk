1841a417f322dc103fef251550aab7aede09d6ea
3Ravaka <120034136+3Ravaka@users.noreply.github.com>
2023-10-23 21:48:03 +0200

    Update Plateau.java

--- MancheRisk/src/Plateau.java
+++ MancheRisk/src/Plateau.java
@@ -11,15 +11,8 @@ public class Plateau{
 		continents[4] = new Continent("Asie", 7);
 		continents[5] = new Continent("Europe", 5);
 		continents[6] = new Continent("Australie", 2);
+
 		//création des territoires
-		//------------------------Europe-----------------------------
-		Territoire islande = new Territoire("Islande");
-		Territoire gB = new Territoire("Grande-Bretagne");
-		Territoire europeOuest = new Territoire("Europe de l’Ouest");
-		Territoire europeSud = new Territoire("Europe de Sud");
-		Territoire europeNord = new Territoire("Europe de Nord");
-		Territoire scandinavie = new Territoire("Scandinavie");
-		Territoire russie = new Territoire("Russie");
 		//------------------------Afrique-----------------------------
 		Territoire egypte = new Territoire("Egypte");
 		Territoire afriqueNord = new Territoire("Afrique du Nord");
@@ -27,11 +20,6 @@ public class Plateau{
 		Territoire afriqueCentrale = new Territoire("Afrique Centrale");
 		Territoire afriqueSud = new Territoire("Afrique du Sud");
 		Territoire madagascar = new Territoire("Madagascar");
-		//----------------------Australie-----------------------------
-		Territoire indonesie = new Territoire("Indonésie");
-		Territoire nouvGuinee = new Territoire("Nouvelle Guinée");
-		Territoire australieOccidentale = new Territoire("Australie Occidentale");
-		Territoire australieOrientale = new Territoire("Australie Orientale");
 		//-------------------Amérique du Nord-------------------------
 		Territoire alaska = new Territoire("Alaska");
 		Territoire territoireNordOuest = new Territoire("Territoire du Nord-Ouest");
@@ -47,17 +35,48 @@ public class Plateau{
 		Territoire bresil = new Territoire("Brésil");
 		Territoire perou = new Territoire("Pérou");
 		Territoire argentine = new Territoire("Argentine");
-
+		//---------------------------Asie----------------------------
+		Territoire oural = new Territoire("Oural");
+		Territoire oural = new Territoire("Sibérie");
+		Territoire oural = new Territoire("Yakoutie");
+		Territoire oural = new Territoire("Kamchatka");
+		Territoire oural = new Territoire("Irkoutsk");
+		Territoire oural = new Territoire("Mongolie");
+		Territoire oural = new Territoire("Japon");
+		Territoire oural = new Territoire("Afghanistan");
+		Territoire oural = new Territoire("Chine");
+		Territoire oural = new Territoire("Moyen-Orient");
+		Territoire oural = new Territoire("Inde");
+		Territoire oural = new Territoire("Asie du Sud-Est");
+		//----------------------Australie-----------------------------
+		Territoire indonesie = new Territoire("Indonésie");
+		Territoire nouvGuinee = new Territoire("Nouvelle Guinée");
+		Territoire australieOccidentale = new Territoire("Australie Occidentale");
+		Territoire australieOrientale = new Territoire("Australie Orientale");
+		//------------------------Europe-----------------------------
+		Territoire islande = new Territoire("Islande");
+		Territoire gB = new Territoire("Grande-Bretagne");
+		Territoire europeOuest = new Territoire("Europe de l’Ouest");
+		Territoire europeSud = new Territoire("Europe de Sud");
+		Territoire europeNord = new Territoire("Europe de Nord");
+		Territoire scandinavie = new Territoire("Scandinavie");
+		Territoire russie = new Territoire("Russie");
 		
 		//intégration des territoires dans les continents
+		//-----------------------Afrique-----------------------------
+		continents[1].getList().
+		//------------------Amérique du Nord-------------------------
+		//-------------------Amérique du Sud-------------------------
+		//------------------------Europe-----------------------------
 		continents[5].getList().add(islande);
 		continents[5].getList().add(gb);
 		continents[5].getList().add(europeOuest);
 		continents[5].getList().add(europeSud);
 		continents[5].getList().add(islande);
 		continents[5].getList().add(islande);
+		
 		//intégration des listes de territoires voisins par territoires 
-		egypte.getList().add(afriqueNord);
+		egypte.getVoisins() = new Territoire[] {afriqueNord, afriqueOrientale, moyenOrient, europeSud}
 		
 	}
 }
