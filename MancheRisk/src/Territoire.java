154c61a6fd889108e32919a6c61a93a13e79ef3c
LLLNN99 <llyynn0323@gmail.com>
2023-10-23 12:09:49 +0200

    Update Territoire.java

--- MancheRisk/src/Territoire.java
+++ MancheRisk/src/Territoire.java
@@ -2,4 +2,11 @@ public class Territoire {
     private String nomTerritoire;
     private int nbTerritoire;
     private Joueur joueurProprietaire; 
+
+
+    public Territoire(){
+        this.nomTerritoire = nomTerritoire;
+        this.nbTerritoire = nbTerritoire;
+        this.joueurProprietaire = joueurProprietaire;
+    }
 }
