3885f4f53aeb96490c5ac368fe818991544d9304
3Ravaka <120034136+3Ravaka@users.noreply.github.com>
2023-10-23 16:16:20 +0200

    Update Plateau.java

--- MancheRisk/src/Plateau.java
+++ MancheRisk/src/Plateau.java
@@ -12,20 +12,28 @@ public class Plateau{
 		continents[5] = new Continent("Europe", 5);
 		continents[6] = new Continent("Australie", 2);
 		//création des territoires
-		Territoire territEurope1 = new Territoire("Islande");
-		Territoire territEurope2 = new Territoire("Grande-Bretagne");
-		Territoire territEurope3 = new Territoire("Europe de l’Ouest");
-		Territoire territEurope4 = new Territoire("Europe de Sud");
-		Territoire territEurope5 = new Territoire("Europe de Nord");
-		Territoire territEurope6 = new Territoire("Scandinavie");
-		Territoire territEurope7 = new Territoire("Russie");
-		Territoire territAfrique1 = new Territoire("");
-		Territoire territAfrique2 = new Territoire("");
-		Territoire territAfrique3 = new Territoire("");
-		Territoire territAfrique4 = new Territoire("");
-		Territoire territAfrique5 = new Territoire("");
-		Territoire territAfrique6 = new Territoire("");
-		Territoire territ
+		//------------------------Europe-----------------------------
+		Territoire Islande = new Territoire("Islande");
+		Territoire GB = new Territoire("Grande-Bretagne");
+		Territoire europeOuest = new Territoire("Europe de l’Ouest");
+		Territoire europeSud = new Territoire("Europe de Sud");
+		Territoire europeNord = new Territoire("Europe de Nord");
+		Territoire scandinavie = new Territoire("Scandinavie");
+		Territoire russie = new Territoire("Russie");
+		//------------------------Afrique-----------------------------
+		Territoire egypte = new Territoire("Egypte");
+		Territoire afriqueNord = new Territoire("Afrique du Nord");
+		Territoire afriqueOrientale = new Territoire("Afrique Orientale");
+		Territoire afriqueCentrale = new Territoire("Afrique Centrale");
+		Territoire afriqueSud = new Territoire("Afrique du Sud");
+		Territoire madagascar = new Territoire("Madagascar");
+		//------------------------Australie-----------------------------
+		Territoire indonesie = new Territoire("Indonésie");
+		Territoire nouvGuinee = new Territoire("Nouvelle Guinée");
+		Territoire australieOccidentale = new Territoire("Australie Occidentale");
+		Territoire australieOrientale = new Territoire("Australie Orientale");
+		//------------------------Amérique du Nord-----------------------------
+		
 
 		
 		//intégration des territoires dans les continents
