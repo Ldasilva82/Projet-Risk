f26503e6a17c65a0aee6b8c97a2e7e12fcde4477
3Ravaka <120034136+3Ravaka@users.noreply.github.com>
2023-10-23 15:18:56 +0200

    Update Plateau.java

--- MancheRisk/src/Plateau.java
+++ MancheRisk/src/Plateau.java
@@ -1,8 +1,9 @@
-public class Plateau {
+public class Plateau{
 	//attribute
 	private Territoire[42] territoire;
 	//constructor
 	public Plateau(){
 		Continent[6]
+		Territoire territ = new Territoire("")
 	}
 }
