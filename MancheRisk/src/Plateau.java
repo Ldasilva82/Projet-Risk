f3d7e74abbfeefa659adda13735e57ce33722e8d
3Ravaka <120034136+3Ravaka@users.noreply.github.com>
2023-10-23 15:40:07 +0200

    Update Plateau.java

--- MancheRisk/src/Plateau.java
+++ MancheRisk/src/Plateau.java
@@ -1,9 +1,20 @@
 public class Plateau{
 	//attribute
-	private Territoire[42] territoire;
+	private Continent[] continents;
 	//constructor
 	public Plateau(){
-		Continent[6]
-		Territoire territ = new Territoire("")
+		//Création des continents 
+		continents = new Continent[6];
+		continents[1] = new Continent("Afrique", 3);
+		continents[2] = new Continent("Amérique du Nord", 5);
+		continents[3] = new Continent("Amérique du Sud" 2);
+		continents[4] = new Continent("Asie", 7);
+		continents[5] = new Continent("Europe", 5);
+		continents[6] = new Continent("Australie", 2);
+		//création des territoires
+		Territoire territEurope = new Territoire("Islande");
+		//intégration des territoires dans les continents
+		continents[5].getList().add(territEurope);
+		
 	}
 }
