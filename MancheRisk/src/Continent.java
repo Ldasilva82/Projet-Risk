dc7d363099d8697664bb8a16de8e9be68b13f8e0
LLLNN99 <llyynn0323@gmail.com>
2023-10-23 15:57:58 +0200

    Update Continent.java

--- MancheRisk/src/Continent.java
+++ MancheRisk/src/Continent.java
@@ -4,6 +4,17 @@ public class Continent {
 	private String nomContinent;
 	private List<Continent> continent;
 
+	public Continent(){
+		this.nomContinent = nomContinent;
+		this.continent = continent;
+	}
+
+	public String getContinent(){
+		return nomContinent;
+	}
+
+
+
     
 
 
