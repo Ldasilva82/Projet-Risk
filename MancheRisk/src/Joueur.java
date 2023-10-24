bd14036b9bd05e4a88d21a265e1621fd7e044871
Ldasilva82 <ldasilva82200@gmail.com>
2023-10-24 10:31:51 +0200

    init classe joueur

--- MancheRisk/src/Joueur.java
+++ MancheRisk/src/Joueur.java
@@ -1,3 +1,38 @@
+import java.util.Random;
+
 public class Joueur {
-    
+	protected String nom;
+	protected String prenom;
+	protected int attaquereussie;
+	protected int defensereussie;
+	protected int nbDé1;
+	protected int territoireConquis;
+	
+	
+	public Joueur(String nom, String prenom) {
+		this.prenom = prenom;
+		this.nom = nom;
+		this.attaquereussie = 0;
+		this.defensereussie = 0;
+		this.nbDé1 = 0;
+		this.territoireConquis = 0;
+	}
+	
+	public int lancerDé() {
+		Random random = new Random();
+	    int value = random.nextInt((6 - 1) + 1) + 1;
+	    if (value==1) {
+	    	this.setNbDé1(nbDé1+1);
+	    }
+	    return value;
+	}
+
+	public int getNbDé1() {
+		return nbDé1;
+	}
+
+	public void setNbDé1(int nbDé1) {
+		this.nbDé1 = nbDé1;
+	}
 }
+	
